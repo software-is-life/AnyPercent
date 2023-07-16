e59d7549f489f7f97106353d77820d9bd299a4c9
software-is-life <kevincarrsoftware@gmail.com>
2023-07-16 10:12:37 -0400

    update data-source

--- api/data-source.ts
+++ api/data-source.ts
@@ -9,10 +9,8 @@ export const AppDataSource = new DataSource({
     port: 3306,
     username: process.env.PLANET_SCALE_USERNAME_LOCAL,
     password: process.env.PLANET_SCALE_PASSWORD_LOCAL,
-    // database: process.env.PLANET_SCALE_DATABASE_LOCAL,
-    // synchronize: true, //false,
-    database: "any-percent", // process.env.PLANET_SCALE_DATABASE_LOCAL,
-    synchronize: false, //false,
+    database: process.env.PLANET_SCALE_DATABASE_LOCAL,
+    synchronize: true, //false,
     // ssl: {
     //     rejectUnauthorized: true,
     // },
